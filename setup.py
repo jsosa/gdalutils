0fd61b20f68d1d02983f9110a36b0d32ab75b32a
jsosa <j.sosa@bristol.ac.uk>
2018-02-09 12:49:36 +0000

    First code commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gdalutils',
+      version='0.1',
+      description='GDAL utils',
+      url='http://github.com/jsosa/gdalutils',
+      author='Jeison Sosa-Moreno',
+      author_email='sosa.jeison@gmil.com',
+      license='MIT',
+      packages=['gdalutils'],
+      zip_safe=False)
