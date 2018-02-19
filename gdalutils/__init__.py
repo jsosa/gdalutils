263d2a7e5979f538aee8fbd5adb0ce3e4d18a973
jsosa <j.sosa@bristol.ac.uk>
2018-02-19 12:06:18 +0000

    Small bug

--- gdalutils/__init__.py
+++ gdalutils/__init__.py
@@ -1,1 +1,6 @@
-import gdalutils
+from gdalutils import get_dataxy
+from gdalutils import get_data
+from gdalutils import get_geo
+from gdalutils import clip_raster
+from gdalutils import write_raster
+
